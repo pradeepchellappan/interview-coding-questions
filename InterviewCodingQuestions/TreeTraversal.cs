84ce50116ebb4b34628d0d12862a37a32c4182da
pradeepchellappan <pc.mail.public@gmail.com>
2014-03-01 12:18:55 -0800

    Added FindNode and FindParentNode

--- InterviewCodingQuestions/TreeTraversal.cs
+++ InterviewCodingQuestions/TreeTraversal.cs
@@ -67,13 +67,121 @@ namespace InterviewCodingQuestions
             InOrderDepthFirstTraverse(tree.Root);
             Console.WriteLine("====================");
 
-            PreOrderDepthFirstTraverse(tree.Root);
-            Console.WriteLine("====================");
+            ////PreOrderDepthFirstTraverse(tree.Root);
+            ////Console.WriteLine("====================");
 
-            PostOrderDepthFirstTraverse(tree.Root);
-            Console.WriteLine("====================");
+            ////PostOrderDepthFirstTraverse(tree.Root);
+            ////Console.WriteLine("====================");
 
-            BreadthFirstTraverse(tree.Root);
+            ////BreadthFirstTraverse(tree.Root);
+            ////Console.WriteLine("====================");
+
+            ////FindParentInBinarySearchTree();
+            ////Console.WriteLine("====================");
+
+            FindNodeInTree();
+            Console.WriteLine("====================");
+        }
+
+        /// <summary>
+        /// Find specified node in a tree.
+        /// </summary>
+        public void FindNodeInTree()
+        {
+            var tree = CreateTree();
+
+            var node = FindNodeInTree(tree, 77);
+
+            Console.WriteLine(node == null ? "Node not found" : "Found node with data " + node.Data);
+        }
+
+        /// <summary>
+        /// Given a tree and some data, find the node that stores the data.
+        /// </summary>
+        /// <typeparam name="T">The type of data stored in the tree nodes.</typeparam>
+        /// <param name="tree">The tree.</param>
+        /// <param name="data">The data which is stored in the node to be found.</param>
+        private Node<T> FindNodeInTree<T>(Tree<T> tree, T data) where T: IComparable<T>
+        {
+            var currentNode = tree.Root;
+
+            while(currentNode != null)
+            {
+                int compareResult = data.CompareTo(currentNode.Data);
+
+                if (compareResult == 0)
+                    return currentNode;
+                else if (compareResult < 0)
+                    currentNode = currentNode.Left;
+                else
+                    currentNode = currentNode.Right;
+            }
+
+            return null; // not found.
+        }
+
+        /// <summary>
+        /// Given a node (or the id of the node), find its parent node.
+        /// </summary>
+        public void FindParentInBinarySearchTree()
+        {
+            var tree = CreateTree();
+
+            try
+            {
+                var parentNode = FindParent(tree.Root, new Node<int>(1));
+                Console.WriteLine(parentNode == null ? "Specified node is root node." : "Parent node = " + parentNode.Data.ToString());
+            }
+            catch(InvalidOperationException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Given the root node of a tree and an arbitrary node in the tree, 
+        /// find the parent node of the arbitrary node.
+        /// This function assumes that the data stored in the nodes are unique (for comparison purposes).
+        /// </summary>
+        /// <typeparam name="T">The type of data stored in the tree nodes.</typeparam>
+        /// <param name="rootNode">The root node</param>
+        /// <param name="childNode">The node whose parent needs to be found</param>
+        /// <returns>
+        /// The parent node. Null is returned if the specified node is the root node (and hence no parent).
+        /// </returns>
+        /// <exception cref="InvalidOperationException">
+        /// Thrown when the specified node was not found in the tree.
+        /// </exception>
+        private Node<T> FindParent<T>(Node<T> rootNode, Node<T> childNode) where T:IComparable<T>
+        {
+            var currentNode = rootNode;
+            Node<T> parentNode = null;
+
+            while (currentNode != null)
+            {
+                int compareValue = childNode.Data.CompareTo(currentNode.Data);
+
+                // Found node.
+                if (compareValue == 0)
+                    return parentNode;
+
+                // Decide whether to go down the left subtree or right subtree.
+                if (compareValue < 0)
+                {
+                    parentNode = currentNode;
+                    currentNode = currentNode.Left;
+                }
+                else
+                {
+                    parentNode = currentNode;
+                    currentNode = currentNode.Right;
+                }
+            }
+
+            if (currentNode == null)
+                throw new InvalidOperationException("The specified not is not in the tree.");
+
+            return parentNode;
         }
 
         /// <summary>
